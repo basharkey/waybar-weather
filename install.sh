94a08da246ed8e76e38cb1f5c6759d72a42916b8
Bradley Sharkey <15859442+basharkey@users.noreply.github.com>
2022-04-14 19:01:29 -0400

    install script updated docs

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+pip3 install -r requirements.txt
+mkdir -p ~/.config/wayweather
+cp ./wayweather.yml ~/.config/wayweather/wayweather.yml
+mkdir ~/bin
+cp ./wayweather.py ~/bin/wayweather.py
