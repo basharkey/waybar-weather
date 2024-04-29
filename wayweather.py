ef0003203def362fcd51070c79d207be7ad2e92c
basharkey <15859442+basharkey@users.noreply.github.com>
2024-04-29 19:37:00 -0400

    Use weather.gc.ca instead

--- wayweather.py
+++ wayweather.py
@@ -11,6 +11,7 @@ config_paths = [
     '.config/wayweather/wayweather.yml',
     '.config/wayweather/wayweather.yaml'
 ]
+
 for path in config_paths:
     config_file = pathlib.Path(pathlib.Path.home(), path)
     if config_file.is_file():
@@ -28,24 +29,27 @@ try:
 except Exception as err:
     raise KeyError("invalid config") from err
 
-
-url = 'https://weather.com/en-IN/weather/hourbyhour/l/' + location_id
+url = f'https://weather.gc.ca/forecast/hourly/on-{location_id}_metric_e.html'
 
 resp = requests.get(url)
 soup = BeautifulSoup(resp.text, 'html.parser')
-hours = soup.find_all('details', id=re.compile('detailIndex.*'))
+hours = soup.find_all('tr')
 
 data = []
 for hour in hours:
-    time = hour.find('h3', attrs={'data-testid':'daypartName'}).text
-    temp = hour.find('span', attrs={'data-testid':'TemperatureValue'}).text
-    precip = hour.find('span', attrs={'data-testid':'PercentageValue'}).text
-    wind = hour.find('span', attrs={'data-testid':'Wind'}).text.split()
-    wind_direction = wind[0]
-    wind_speed = wind[1]
-    data.append({'time': time, 'temp': temp, 'precip': precip, 'wind_direction': wind_direction, 'wind_speed': wind_speed})
+    try:
+        time = hour.find('td', attrs={'headers':'header1'}).text.strip(' ')
+        temp = hour.find('td', attrs={'headers':'header2'}).text.strip(' ')
+        cond = hour.find('td', attrs={'headers':'header3'}).text.strip(' ')
+        precip = hour.find('td', attrs={'headers':'header4'}).text.strip(' ')
+        wind = hour.find('td', attrs={'headers':'header5'}).text.split(' ', maxsplit=1)
+        wind_direction = wind[0].strip(' ')
+        wind_speed = wind[1].strip(' ')
+        data.append({'time': time, 'temp': temp, 'precip': precip, 'wind_direction': wind_direction, 'wind_speed': wind_speed})
+    except:
+        pass
 
 print("|", end="")
 for i in range(output_len):
-    print(f" {data[i]['time']} {data[i]['temp']:>4} {data[i]['precip']:>4} {data[i]['wind_direction']:>3} {data[i]['wind_speed']:>3} km/h", end=" |")
+    print(f" {data[i]['time']} {data[i]['temp']:>4}° {data[i]['precip']:>4}% {data[i]['wind_direction']:>3} {data[i]['wind_speed']:>3} km/h", end=" |")
 
