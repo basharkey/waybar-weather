91ff49ca49bfc7aed1c9a1e851380af025062687
basharkey <15859442+basharkey@users.noreply.github.com>
2024-08-23 11:25:03 -0400

    fix compatibility with new lat/lon weather site

--- wayweather.py
+++ wayweather.py
@@ -25,11 +25,12 @@ except Exception as err:
 
 try:
     output_len = config['output_length']
-    location_id = config['location_id']
+    latitude = config['latitude']
+    longitude = config['longitude']
 except Exception as err:
     raise KeyError("invalid config") from err
 
-url = f'https://weather.gc.ca/forecast/hourly/on-{location_id}_metric_e.html'
+url = f'https://weather.gc.ca/en/forecast/hourly/index.html?coords={latitude},{longitude}'
 
 resp = requests.get(url)
 soup = BeautifulSoup(resp.text, 'html.parser')
@@ -42,7 +43,8 @@ for hour in hours:
         temp = hour.find('td', attrs={'headers':'header2'}).text.strip(' ')
         cond = hour.find('td', attrs={'headers':'header3'}).text.strip(' ')
         precip = hour.find('td', attrs={'headers':'header4'}).text.strip(' ')
-        wind = hour.find('td', attrs={'headers':'header5'}).text.split(' ')
+        uv_index = hour.find('td', attrs={'headers':'header5'}).text.strip(' ')
+        wind = hour.find('td', attrs={'headers':'header6'}).text.split(' ')
         wind_direction = wind[0]
         wind_speed = wind[1]
         data.append({'time': time, 'temp': temp, 'precip': precip, 'wind_direction': wind_direction, 'wind_speed': wind_speed})
@@ -52,4 +54,3 @@ for hour in hours:
 print("|", end="")
 for i in range(output_len):
     print(f" {data[i]['time']} {data[i]['temp']:>3}° {data[i]['precip']:>3}% {data[i]['wind_direction']:>2} {data[i]['wind_speed']:>2} km/h", end=" |")
-
